db5345226282294fbb951074433010718a673c7a
Asser Elshafey <elshafeyasser@yahoo.com>
2023-06-27 11:26:31 -0700

    HOT FIX

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """defines class square """
-python3 -c 'print(__import__("my_module").__doc__)'
-    class Square:
-        python3 -c 'print(__import__("my_module").MyClass.__doc__)'
-        pass
+class Square:
+    """An empty class that represents a square."""
+    pass
