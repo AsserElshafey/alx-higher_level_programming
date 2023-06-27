81db32fbe43d657ec899961dfaa593d5f91444c7
Asser Elshafey <elshafeyasser@yahoo.com>
2023-06-27 11:19:30 -0700

    HOT FIX

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,17 +1,6 @@
 #!/usr/bin/python3
 """defines class square """
-
+python3 -c 'print(__import__("my_module").__doc__)'
     class Square:
-    """An empty class that represents a square.
-
-    Attributes:
-        side: The length of the side of the square.
-    """
-
-    def __init__(self, side: float) -> None:
-        """Initializes a square with a given side length.
-
-        Args:
-            side: The length of the side of the square.
-        """
-        self.side = side
+        python3 -c 'print(__import__("my_module").MyClass.__doc__)'
+        pass
