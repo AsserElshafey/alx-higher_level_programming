34b863b2716f01c3078b94efe3f6ba5247eb8b5c
Asser Elshafey <elshafeyasser@yahoo.com>
2023-07-10 08:10:19 -0700

    task4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,6 +6,7 @@ Contains the inherits_from function
 
 
 def inherits_from(obj, a_class):
-    """returns true if obj is a subclass of a_class, otherwise false"""
 
-    return(issubclass(type(obj), a_class) and type(obj) != a_class)
+    obj_type = type(obj)
+    is_class = issubclass(obj_type, a_class) and obj_type is not a_class
+    return is_class
