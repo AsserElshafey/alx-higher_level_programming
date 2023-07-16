16b3440cdd938f6b097f2f0253c2e38c19c81499
Asser Elshafey <elshafeyasser@yahoo.com>
2023-07-16 20:59:39 +0300

    test rectangle

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -7,8 +7,6 @@ Unittest classes:
     TestBase_from_json_string - line 234
     TestBase_create - line 288
     TestBase_load_from_file - line 340
-    TestBase_save_to_file_csv - line 406
-    TestBase_load_from_file_csv - line 484
 """
 import os
 import unittest
