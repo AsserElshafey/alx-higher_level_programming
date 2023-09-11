72061b50d449a73df91e52784b5ee82909d96fc9
Asser Elshafey <elshafeyasser@yahoo.com>
2023-09-11 19:49:55 +0300

    task5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 const firstArg = process.argv[2];
 
 if (firstArg) {
-  console.log('My number: ' + Number(firstArg));
+  console.log('My number: ' + parseInt(firstArg));
 } else {
   console.log('Not a number');
 }
