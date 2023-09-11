c860e880b1feb1fcae4e6f3604d9cfcd99a32dcd
Asser Elshafey <elshafeyasser@yahoo.com>
2023-09-11 19:52:07 +0300

    task5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,7 +4,7 @@ let firstArg = process.argv[2];
 let num = parseInt(firstArg);
 
 if (isNaN(num)) {
-  console.log("Not a number");
+  console.log('Not a number');
 } else {
-  console.log("My number: " + num);
+  console.log('My number: ' + num);
 }
