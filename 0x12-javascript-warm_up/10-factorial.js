ca9f87f88ca0ecd87669be12f5177e31b9ea61dc
Asser Elshafey <elshafeyasser@yahoo.com>
2023-09-11 20:28:53 +0300

    task10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+function factorial (n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+const firstArg = process.argv[2];
+
+const num = parseInt(firstArg);
+
+console.log(factorial(num));
